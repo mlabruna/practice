3089a975d4c011ecd62d4e28a4978e9cee6a8439
mlabruna <Maria@White-MacBook-3.local>
2013-11-05 20:11:43 -0800

    Challenge = Square Roots

---
+++ udemy/UdemyPractice/src/com/practice/challenge/SquareRoot.java
@@ -0,0 +1,16 @@
+package com.practice.challenge;
+
+import java.util.Scanner;
+
+public class SquareRoot {
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Please enter a number: ");
+		
+		float value = scanner.nextFloat();
+		
+		System.out.println(value*value);
+		
+	}
+}
