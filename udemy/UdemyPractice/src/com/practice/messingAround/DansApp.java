2e463526abdff19eb73e9d826128b1dba66e62ac
mlabruna <Maria@White-MacBook-3.local>
2013-11-25 19:58:48 -0800

    Created a small program for Dan's App
    created a small program where the user is asked for an item using
    Scanner which them labels that item as a variable and adds it to the
    list.
    
    In progress: trying to make the program work where all of the past
    items are saved as variables in the list

---
+++ udemy/UdemyPractice/src/com/practice/messingAround/DansApp.java
@@ -0,0 +1,33 @@
+package com.practice.messingAround;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+//imported scanner, list, and arrayList systems
+
+public class DansApp {
+//named the blueprint
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner input = new Scanner(System.in);
+		System.out.println("What would you like to add to the list: ");
+		
+		String newItem = input.nextLine();
+		
+		System.out.println(newItem + " have been added");
+		
+		List<String> list = new ArrayList<String>();
+		
+		list.add(newItem);
+		assert newItem == list.get(0);
+		
+		/*List myList = new List();
+		myList<value>
+		*/
+		System.out.println(list);
+		
+	}
+
+}
