70f3a37b11c614ef473e6bb995b77606d4744d74
mlabruna <Maria@White-MacBook-3.local>
2013-12-01 21:43:01 -0800

    Udemy tutorials 9 & 10 with package info

---
+++ udemy/UdemyPractice/src/com/practice/udemy/tutorial10/Application10.java
@@ -0,0 +1,23 @@
+package com.practice.udemy.tutorial10;
+
+//import java.util.Scanner;
+
+public class Application10 {
+	public static void main(String[] args) {
+		int value = 7;
+		
+		int[] values;
+		values = new int[3];
+		
+		System.out.println(values[0]);
+		
+		values[0] = 10;
+		values[1] = 20;
+		values[2] = 30;
+
+		System.out.println(values[0]);
+		System.out.println(values[1]);
+		System.out.println(values[2]);
+
+	}
+}
