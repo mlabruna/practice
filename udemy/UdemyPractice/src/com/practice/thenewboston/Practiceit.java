7e386f9d96568bf19ddc442d5e08fd81c6d0538f
mlabruna <Maria@White-MacBook-3.local>
2014-03-30 20:21:20 -0700

    theNewBoston video 16
    I am not sure what i have done. I know its super basic but i fell
    REALLY behind. Can you help?

---
+++ udemy/UdemyPractice/src/com/practice/thenewboston/Practiceit.java
@@ -0,0 +1,21 @@
+package com.practice.thenewboston;
+
+import java.util.Scanner;
+
+class Practiceit {
+	public static void main (String[] args) {
+		Scanner input = new Scanner(System.in);
+		review reviewObject = new review();
+		System.out.println("Enter name of first friend: ");
+		String friend = input next.Line();
+ 		reviewObject.setName(friend);
+ 		reviewObject.saying();
+	}
+/*	public static void main(String[] args) {
+		System.out.println("\ta\tb\tc");
+		System.out.println("\\\\");
+		System.out.println("'");
+		System.out.println("C:\nin\the downward spiral");
+		
+	}*/
+}
---
+++ udemy/UdemyPractice/src/com/practice/thenewboston/review.java
@@ -0,0 +1,16 @@
+package com.practice.thenewboston;
+
+public class review {
+		private String girlName;
+		public void setName(String name) {
+			girlName = name;
+		}
+		public String getName() {
+			return girlName;
+		}
+		public void saying () {
+		System.out.printf("your first friend was %s", getName());
+
+	}
+
+}
